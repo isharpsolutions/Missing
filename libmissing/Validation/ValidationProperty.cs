b3a28890f76690bde2186782102ae6faa20326e9
Patrick Timm <patrick@isharp.dk>
2012-03-14 15:25:00 +0100

    Added documentation. Changed protection on "IsRequired" from public to internal (to reduce clutter during "mapping").

--- libmissing/Validation/ValidationProperty.cs
+++ libmissing/Validation/ValidationProperty.cs
@@ -4,15 +4,27 @@ using System.Collections.Generic;
 
 namespace Missing.Validation
 {
+	/// <summary>
+	/// Validation property
+	/// </summary>
 	public class ValidationProperty
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Missing.Validation.ValidationProperty"/> class.
+		/// </summary>
 		public ValidationProperty()
 		{
 			this.PropertyPath = new List<string>();
 		}
 		
+		/// <summary>
+		/// Get/set the property path
+		/// </summary>
 		public IList<string> PropertyPath { get; set; }
 		
+		/// <summary>
+		/// Get the name of the underlying property
+		/// </summary>
 		public string Name
 		{
 			get
@@ -24,12 +36,22 @@ namespace Missing.Validation
 		#region Required
 		private bool isRequired = false;
 		
-		public bool IsRequired
+		/// <summary>
+		/// Get/set whether this property is required
+		/// </summary>
+		internal bool IsRequired
 		{
 			get { return this.isRequired; }
 			set { this.isRequired = value; }
 		}
 		
+		/// <summary>
+		/// Marks this property as being required
+		/// </summary>
+		/// <remarks>
+		/// Currently we do not support "Required"
+		/// for properties of types where default(..type..) = null
+		/// </remarks>
 		public ValidationProperty Required()
 		{
 			this.IsRequired = true;
