51f1edba1051eef0fd6620062c5661c258f96eb9
Patrick Timm <patrick@isharp.dk>
2012-03-15 11:37:43 +0100

    Added a property called "MyEmail" which is not required, but uses the EmailEnforcer. Added tests for that case.

--- libmissing-tests/Validation/ValidationSpecificationTests_SimpleModel.cs
+++ libmissing-tests/Validation/ValidationSpecificationTests_SimpleModel.cs
@@ -18,6 +18,8 @@ namespace Missing
 		public decimal MyDecimal { get; set; }
 		public float MyFloat { get; set; }
 		public double MyDouble { get; set; }
+		
+		public string MyEmail { get; set; }
 	}
 	#endregion Model
 	
@@ -30,9 +32,20 @@ namespace Missing
 				.Required()
 				.Length(10);
 			
-			
 			base.Field(y => y.MyInt)
 				.Required();
+			
+			base.Field(y => y.MyEmail)
+				.AllowedEmail();
+			
+			//
+			// make a test where email is not required
+			// but still calls AllowedEmail
+			//
+			// not calling required should make the
+			// field continue validation only if
+			// the field contains a value
+			//
 		}
 	}
 	#endregion Validation spec
@@ -47,6 +60,9 @@ namespace Missing
 			SimpleModel model = new SimpleModel();
 			model.MyString = "Something";
 			model.MyInt = 29;
+			
+			// MyEmail is not required, hence we do not specify it
+			// model.MyEmail = "user@domain.tld";
 			
 			ValidationResult result = Validator.Validate<SimpleModel>(model);
 			
@@ -107,6 +123,34 @@ namespace Missing
 			Assert.AreEqual(1, result.Errors.Count, "There should be 1 error");
 			
 			Assert.AreEqual("MyString", result.Errors[0].PropertyName, "The property name is wrong");
+		}
+		
+		[Test]
+		public void SimpleModel_EmailValid()
+		{
+			SimpleModel model = new SimpleModel();
+			model.MyString = "Something";
+			model.MyInt = 29;
+			model.MyEmail = "user@domain.tld";
+			
+			ValidationResult result = Validator.Validate<SimpleModel>(model);
+			
+			Assert.IsFalse(result.HasErrors(), "There should not be any errors");
+		}
+		
+		[Test]
+		public void SimpleModel_EmailInvalid()
+		{
+			SimpleModel model = new SimpleModel();
+			model.MyString = "Something";
+			model.MyInt = 29;
+			model.MyEmail = "user";
+			
+			ValidationResult result = Validator.Validate<SimpleModel>(model);
+			
+			Assert.AreEqual(1, result.Errors.Count, "There should be 1 error");
+			
+			Assert.AreEqual("MyEmail", result.Errors[0].PropertyName, "The property name is wrong");
 		}
 	}
 }
