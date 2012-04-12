2213c85a94d781db96605cfdade4cfa36555ebe2
Nicklas Overgaard <nicklas@isharp.dk>
2012-04-12 23:30:36 +0200

    Created PasswordStrengScore enumeration and an empty class for determining the score of a given password.

---
+++ libmissing/Security/PasswordStrenghtScore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Missing.Security
+{
+	public enum PasswordStrenghtScore
+	{
+		/// <summary>
+		/// The strenght of the password is non existant
+		/// </summary>
+		Useless,
+
+		/// <summary>
+		/// The password is weak and will be easily cracked / guessed by an attacker
+		/// </summary>
+		Weak,
+
+		Okay,
+
+		Great
+	}
+}
