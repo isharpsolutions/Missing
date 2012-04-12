8448f063dc0dac145cfddc22a1507374e01fc24e
Nicklas Overgaard <nicklas@isharp.dk>
2012-04-12 22:02:38 +0200

    Created stringextensions for checking if a string contains only lowercase letters, or capital letters and so forth.
    All methods currently throws NotImplementedException.

---
+++ libmissing/String/Extensions/StringExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Missing.String.Extensions
+{
+	public static class StringExtensions
+	{
+
+		/// <summary>
+		/// Determines whether this string contains only alpha lowercase symbols
+		/// </summary>
+		/// <param name="str">The string</param>
+		/// <returns>
+		///   <c>true</c> if [contains only alpha lowercase]; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool ContainsOnlyAlphaLowercase(this string str)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Determines whether this string contains only alpha capital symbols
+		/// </summary>
+		/// <param name="str">The STR.</param>
+		/// <returns>
+		///   <c>true</c> if [contains only alpha capital]; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool ContainsOnlyAlphaCapital(this string str)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Determines whether this string contains only numbers
+		/// </summary>
+		/// <param name="str">The STR.</param>
+		/// <returns>
+		///   <c>true</c> if [contains only numbers] ; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool ContainsOnlyNumbers(this string str)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Determines whether this string contains only symbols
+		/// </summary>
+		/// <param name="str">The STR.</param>
+		/// <returns>
+		///   <c>true</c> if [contains only symbols]; otherwise, <c>false</c>.
+		/// </returns>
+		public static bool ContainsOnlySymbols(this string str)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
