57cc3083c36c4607cacdf55eb0d14f75409ceaea
Nicklas Overgaard <nicklas@isharp.dk>
2012-04-24 18:26:38 +0300

    Created static class RepositoryRetriever, based on the EntityRetriever from S#arp lite.
    Bumped version

---
+++ libmissing/Data/Persistance/RepositoryRetriever.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Missing.Data.Persistance.DataInterfaces;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Missing.Data.Persistance
+{
+    public static class RepositoryRetriever
+    {
+        public static object CreateEntityRepositoryFor(Type entityType, Type idType)
+        {
+            Type concreteRepositoryType = typeof(IRepositoryWithTypedId<,>)
+                .MakeGenericType(new[] { entityType, idType });
+
+            object repository = ServiceLocator.Current.GetService(concreteRepositoryType);
+
+            if (repository == null)
+                throw new TypeLoadException(concreteRepositoryType.ToString() + " has not been registered with IoC");
+
+            return repository;
+        }
+    }
+}
