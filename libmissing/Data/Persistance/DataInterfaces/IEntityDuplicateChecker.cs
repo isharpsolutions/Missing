bb6a7490279cd77f493f46809147840895502aef
Nicklas Overgaard <nicklas@isharp.dk>
2012-04-24 15:49:49 +0300

    Initial port of basic classes from S#arp lite. They are actually just copies with namespace changes.

---
+++ libmissing/Data/Persistance/DataInterfaces/IEntityDuplicateChecker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Missing.Data.Persistance.DataInterfaces
+{
+    public interface IEntityDuplicateChecker
+    {
+        bool DoesDuplicateExistWithTypedIdOf<TId>(IEntityWithTypedId<TId> entity);
+    }
+}
---
+++ libmissing/Data/Persistance/DomainSignatureAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Missing.Data.Persistance
+{
+    /// <summary>
+    ///     Facilitates indicating which property(s) describe the unique signature of an 
+    ///     entity.  See EntityWithTypedId.GetTypeSpecificSignatureProperties() for when this is leveraged.
+    /// </summary>
+    /// <remarks>
+    ///     This is intended for use with <see cref = "EntityWithTypedId" />.
+    /// </remarks>
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class DomainSignatureAttribute : Attribute { }
+}
