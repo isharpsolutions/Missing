8cae262c3102b3f57196bcd8a765c618acd2b3f3
Nicklas Overgaard <nicklas@isharp.dk>
2012-05-03 12:58:48 +0200

    Created sub namespace for all this key derivation stuff.
    Added new DTO, DerivedKey, which should be the return from a key derivation function.

---
+++ libmissing/Security/KeyDerivation/DerivedKey.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Missing.Security
+{
+	/// <summary>
+	/// Contains all neccesary information about a key that has been derived using
+	/// the <see cref="KeyDerivation"/> functions
+	/// </summary>
+	public class DerivedKey
+	{
+		public byte[] Salt { get; set; }
+
+		public byte[] Key { get; set; }
+
+		public string KeyHex { get; set; }
+
+		public int Iterations { get; set; }
+	}
+}
