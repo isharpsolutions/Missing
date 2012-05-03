35acc544bbd1c334ecb2b5d305ab7b4526ce94b6
Nicklas Overgaard <nicklas@isharp.dk>
2012-05-03 15:07:37 +0200

    DerivedKey is now in the correct namespace.

--- libmissing/Security/KeyDerivation/DerivedKey.cs
+++ libmissing/Security/KeyDerivation/DerivedKey.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Missing.Security
+namespace Missing.Security.KeyDerivation
 {
 	/// <summary>
 	/// Contains all neccesary information about a key that has been derived using
@@ -18,5 +18,7 @@ namespace Missing.Security
 		public string KeyHex { get; set; }
 
 		public int Iterations { get; set; }
+
+		public KeyDeriverAlgorithm Algorithm { get; set; }
 	}
 }
