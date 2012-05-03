0e53397ce0314ed7a763a2fbfbc8ceb0ce66b9c6
Nicklas Overgaard <nicklas@isharp.dk>
2012-05-03 12:33:21 +0200

    Created initial static class "KeyDerivation" which is to contain the methods for deriving hashes off passwords.
    Added unittests of the, not yet implemented, RandomSalt method.

---
+++ libmissing-tests/Security/KeyDerivationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Missing.Security;
+
+namespace Missing.Security
+{
+	[TestFixture]
+	public class KeyDerivationTests
+	{
+		[Test]
+		public void TestGetSalt()
+		{
+			byte[]salt = KeyDerivation.RandomSalt();
+			// default get salt uses 128 bits, that's 16 bytes
+			Assert.IsTrue(salt.Length == 128/8, "Incorrect salt length");
+		}
+
+		[Test]
+		public void TestGetSaltLargeBitSize()
+		{
+			byte[] salt = KeyDerivation.RandomSalt(1024);
+			Assert.IsTrue(salt.Length == 1024 / 8, "Incorrect salt length");
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void TestGetSaltIncorrectBitSize()
+		{
+			byte[] salt = KeyDerivation.RandomSalt(801);
+			Assert.Fail("Exception of type ArgumentException was not thrown, which is an error");
+		}
+	}
+}
---
+++ libmissing/Security/KeyDerivation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Missing.Security
+{
+	/// <summary>
+	/// Implements the rfc2898 key derivation function PBKDF2
+	/// </summary>
+	public static class KeyDerivation
+	{
+
+		/// <summary>
+		/// Gets a new randomly generated salt of length 128 bits
+		/// </summary>
+		/// <returns></returns>
+		public static byte[] RandomSalt()
+		{
+			return RandomSalt(128);
+		}
+
+
+		/// <summary>
+		/// Gets a new randomly generated salt of the given bit length
+		/// <paramref name="length"/>
+		/// </summary>
+		/// <param name="length">The length, in bits, of the returned salt. Must be a multiple of 8</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException">If <paramref name="length"/> is not a multiple of 8</exception>
+		public static byte[] RandomSalt(int length)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
