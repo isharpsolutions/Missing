abbe07f2590b69b86ec24cca63d3b7e99e44fede
Nicklas Overgaard <nicklas@isharp.dk>
2012-05-23 16:33:16 +0200

    Created new enumeration, "RecordState" which is to be used on Entities.
    The idea behind this enumeration is to support enterprise usecases where
    entities should never be deleted from the database, but merely flagged as
    deleted.

---
+++ libmissing/Data/Persistance/RecordState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Missing.Data.Persistance
+{
+	public enum RecordState
+	{
+		/// <summary>
+		/// Entity is in an active and usable state
+		/// </summary>
+		Active,
+
+		/// <summary>
+		/// Entity is non active but still usable
+		/// </summary>
+		NonActive,
+
+		/// <summary>
+		/// Entity is marked as deleted and should not be used
+		/// </summary>
+		Deleted
+	}
+}
