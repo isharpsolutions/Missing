f568f2e65ebc938ad5822c785ba32e526535e073
Nicklas Overgaard <nicklas@isharp.dk>
2012-09-06 19:42:25 +0200

    Updated version number to reflect that new namespaces have been added (the password derivation stuff)

--- VersionNumber.cs
+++ VersionNumber.cs
@@ -11,4 +11,4 @@ using System.Runtime.CompilerServices;
  * Additions in existing namespaces bumps the minor
  * Bug fixes and misc that does not change the public API bump the patch
  */
-[assembly: AssemblyVersion("0.16.0")]
+[assembly: AssemblyVersion("0.17.0")]
