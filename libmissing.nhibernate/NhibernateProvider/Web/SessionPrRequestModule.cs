3547b5a8e20674a784e0e5d26ec77f99a1b6f39c
Patrick <pingvinen@isflagen.dk>
2013-02-07 18:26:20 -0500

    It is now possible to change the ISession.FlushMode using the config file.

--- libmissing.nhibernate/NhibernateProvider/Web/SessionPrRequestModule.cs
+++ libmissing.nhibernate/NhibernateProvider/Web/SessionPrRequestModule.cs
@@ -4,6 +4,7 @@ using NHibernate;
 using System.Collections.Generic;
 using Microsoft.Practices.ServiceLocation;
 using System.Linq;
+using System.Configuration;
 
 namespace Missing.NhibernateProvider.Web
 {
@@ -12,10 +13,61 @@ namespace Missing.NhibernateProvider.Web
     /// </summary>
     public class SessionPerRequestModule : IHttpModule
     {
+		/// <summary>
+		/// The name of the appSettings key used to control NHibernate ISession.FlushMode
+		/// </summary>
+		public static string SessionFlushModeConfigKey = "Missing.NHibernate.ISession.FlushMode";
+
+		/// <summary>
+		/// Whether the flushmode has been defined by looking at config
+		/// </summary>
+		private static bool SessionFlushModeIsSet = false;
+
+		/// <summary>
+		/// The flushmode to use
+		/// </summary>
+		private static FlushMode SessionFlushMode = FlushMode.Auto;
+
         public void Init(HttpApplication context)
         {
             context.BeginRequest += ContextBeginRequest;
             context.EndRequest += ContextEndRequest;
+
+			if (!SessionFlushModeIsSet)
+			{
+				SessionFlushModeIsSet = true;
+
+				string fm = ConfigurationManager.AppSettings.Get(SessionFlushModeConfigKey);
+
+				if (!String.IsNullOrEmpty(fm))
+				{
+					//
+					// let exceptions bubble through, so developers using this
+					// will see the error
+					//
+					try
+					{
+						SessionFlushMode = (FlushMode)Enum.Parse(typeof(FlushMode), fm, true);
+						Missing.Diagnostics.Log.Information("Session.FlushMode value of '{0}' defined in *.config by appSettings.{1}", SessionFlushMode, SessionFlushModeConfigKey);
+					}
+
+					catch (Exception ex)
+					{
+						//
+						// found this pattern at
+						// http://stackoverflow.com/a/136092
+						//
+
+						if (ex is ArgumentException || ex is OverflowException)
+						{
+							string names = String.Join(", ", Enum.GetNames(typeof(FlushMode)));
+							throw new ArgumentException(String.Format("The value of appSettings.{1} must be one of {2}, but was '{3}'", SessionFlushModeConfigKey, names, fm), ex);
+						}
+
+						throw;
+					}
+				}
+			}
         }
 
         private void ContextBeginRequest(object sender, EventArgs e)
@@ -31,6 +83,9 @@ namespace Missing.NhibernateProvider.Web
         private static ISession BeginSession(ISessionFactory sessionFactory)
         {
             var session = sessionFactory.OpenSession();
+
+			session.FlushMode = SessionFlushMode;
+
             session.BeginTransaction();
             return session;
         }
@@ -47,7 +102,7 @@ namespace Missing.NhibernateProvider.Web
 
         private static void EndSession(ISession session)
         {
-            if (session.Transaction != null && session.Transaction.IsActive)
+			if (session.Transaction != null && session.Transaction.IsActive)
             {
                 session.Transaction.Commit();
             }
